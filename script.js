78bbc38a562d9f659b945cdb838c3e6abc295104
Steve Aquillano <steveaquillano@gmail.com>
2011-07-02 21:46:44 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#vr-proto-header").hide();
+  $("#nav").css({"margin":"0 25px 40px 24px"});
+  $("#mainbody").css({"marginLeft":"190px"});
+  $(".applogo").css({"left":"24px"});
+});
